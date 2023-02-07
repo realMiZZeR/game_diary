535db4802a40420689a13f7e7a0d3e8586f4114f
MiZZeR <87935999+realMiZZeR@users.noreply.github.com>
2023-02-07 22:45:50 +0700

    Made some settings porject (scss, redux, fonts)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import store from '@/store';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
+
+export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ru">
+      <Head>
+      </Head>
       <body>
         <Main />
         <NextScript />
